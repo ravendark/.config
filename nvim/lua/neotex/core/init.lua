6ffd4d65b8ebce648c9c289dee07bb25b4427cc0
Scott Keeler <ravendarkrv@icloud.com>
2025-05-25 11:24:00 -0700

    seems to be working

--- nvim/lua/neotex/core/init.lua
+++ nvim/lua/neotex/core/init.lua
@@ -2,6 +2,8 @@ require("neotex.core.autocmds")
 require("neotex.core.functions")
 require("neotex.core.keymaps")
 require("neotex.core.options")
-
+local function getWords()
+  return tostring(fim.fn.wordcount().words)
+end
 -- Load Avante support module
 require("neotex.plugins.ai.avante-support")
